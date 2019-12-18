36db457a9a0d5996af3dc8e5d3da740e3f608bee
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-18 12:46:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,10 @@
 def revere_each_word(sentence)
-  
+  new = []
+  counter = 0 
+  while counter < sentence.length do
+    new.push (yield(sentence)[counter])
+    counter += 1 
+  end
+  new 
+end
+
