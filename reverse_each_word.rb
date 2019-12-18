62e26a1b86d0583b20c59c2c702f262ca93eadb8
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-18 12:54:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_array = sentence.split("")
-  new_array.each do 
-    { |b| "#{b.reverse}" }
+  new_array.each do |word|
+    puts "#{b.reverse}"
   end
   
 end
